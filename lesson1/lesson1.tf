faab0891d2eef8746ee69ecc8b994e11ac462189
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-08-02 03:51:42 -0400

    terraform repo

---
+++ lesson1/lesson1.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+
+
+resource "aws_instance" "my_instance" {
+  count = 3 
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "nginx"
+  }
+}
+
+
