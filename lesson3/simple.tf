a88f8a06cbe13f09a918a5d5037b21b29d62f93f
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2023-08-03 08:14:39 -0400

    terraform destroy command

---
+++ lesson3/simple.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~>5.10.0"
+      }
+    }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "us-east-1"
+  
+}
+
+resource "aws_instance" "victor" {
+  count = 3
+  ami = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "my_instance"
+    Owner = "Victor"
+    Project = "Terraform Lesson"
+  }
+}
+
+#count = 0  destroy current server , if  count = 3  change count = 2 delete one server out three 
+#destroy command   "terraform destroy"
